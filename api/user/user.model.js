61d61c6ae14f1065028ff750a75c2e6aead1d7d1
Jois Andresky <jois@lighthouse.co.id>
2019-08-24 18:43:32 +0700

    change user model

--- api/user/user.model.js
+++ api/user/user.model.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  nomor_induk: { type: String, trim: true },
   email: { type: String, required: true, trim: true },
   password: { type: String, required: true, trim: true },
-  role: { type: String, required: true, trim: true }
+  role: { type: String, enum: ["Lecture", "Student", "Operational"], required: true, trim: true }
 });
 
 module.exports = mongoose.model('User', UserSchema);
